60f5fe3dd3ea91b90938355fbfd21e5338cd6c64
a7lam <ahlam.adda@hva.nl>
2026-09-17 15:51:02 +0200

    #4 feat✨: dynamic person info sections added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,15 @@
 
 
 <main>
-    <section>
-
+    <section class="name-image-wrapper">
         <img src={data.person.image} alt={data.person.name} class="mugshot" width="300" height="200" />
-        
-
+        <h1>{data.person.name}</h1>
+        <h2>Frontend Developer</h2>
+        <p>Still Learning</p>
     </section>
+
+    <section class="bio">
+        <h2>About me</h2>
+        <p>{ data.person.bio}</p>
+    </section>  
 </main>
