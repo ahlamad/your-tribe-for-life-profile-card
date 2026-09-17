a31d7fb0fcdc559c3e4ba1a57f0d378570a5a38d
a7lam <ahlam.adda@hva.nl>
2026-09-17 16:47:32 +0200

    #4 feat✨: css styling added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,11 @@
 <main>
     <section class="name-image-wrapper">
         <img src={data.person.image} alt={data.person.name} class="mugshot" width="300" height="200" />
-        <h1>{data.person.name}</h1>
-        <h2>Frontend Developer</h2>
-        <p>Still Learning</p>
+        <div>
+          <h1>{data.person.name}</h1>
+          <h2>Frontend Developer</h2>
+          <p>Still Learning</p>
+        </div>
     </section>
 
     <section class="bio">
@@ -16,3 +18,59 @@
         <p>{ data.person.bio}</p>
     </section>  
 </main>
+
+<style>
+
+  section {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .name-image-wrapper {
+    place-items: center;
+
+    &:first-of-type {
+      gap: 1rem;
+    }
+  }
+
+  img {
+    border-radius: 100%;
+    width: 30dvw;
+    height: auto;
+    object-fit: cover;
+    aspect-ratio: 1 / 1;
+    box-shadow: #DC4E79 0 0 12px 4px ;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: .3rem;
+    place-items: center;
+  }
+
+  h1 {
+    font-size: 23px;
+  }
+
+  h2 {
+    font-size: 19px;
+    font-weight: 500;
+  }
+
+  .bio {
+    margin-top: 2rem;
+    place-items: center;
+    text-align: center;
+    gap: .5rem;
+  }
+
+  .bio p {
+    font-size: 16px;
+    font-weight: 300;
+    line-height: 1.4;
+    padding: 0 12px 0 12px;
+  }
+
+</style>
